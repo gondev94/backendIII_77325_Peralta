655405f1b7105127a66fe47dd739eb86e5283530
Gonzalo Peralta <gonnzaperalta94@gmail.com>
2026-05-14 19:28:54 +0200

    feat add logic in service.js

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -19,8 +19,7 @@ export class userService {
         if (existingEmail) {
             throw new Error("Email already in use");
         }
-        const existingUsername =
-            this.userRepository.findUserByUsername(username);
+        const existingUsername = this.userRepository.findUserByUsername(username);
         if (existingUsername) {
             throw new Error("Username already in use");
         }
@@ -31,6 +30,47 @@ export class userService {
             username,
         });
         return newUser;
+    }
+
+    deleteUser(id) {
+        const user = this.userRepository.deleteUser(id);
+        if (!user) {
+            throw new Error("User not found");
+        }
+        return user;
+    }
+
+    updateUser(id, email, password, username) {
+        const user = this.userRepository.findUserById(id);
+        if (!user) {
+            throw new Error("User not found");
+        }
+
+        if (email && email !== user.email) {
+            const existingEmail = this.userRepository.findUserByEmail(email);
+            if (existingEmail) {
+                throw new Error("Email already in use");
+            }
+        }
+
+        if (username && username !== user.username) {
+            const existingUsername = this.userRepository.findUserByUsername(username);
+            if (existingUsername) {
+                throw new Error("Username already in use");
+            }
+        }
+
+        const hashedPassword = password ? bcrypt.hashSync(password, SALT_ROUND) : user.password;
+        return this.userRepository.updateUser(id, email || user.email, hashedPassword, username || user.username)
+
+    }
+
+    getUserById(id) {
+        const user = this.userRepository.findUserById(id);
+        if (!user) {
+            throw new Error("User not found");
+        }
+        return { id: user.id, email: user.email, username: user.username };
     }
 
     loginUser(email, password) {
