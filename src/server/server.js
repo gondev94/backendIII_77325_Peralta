e29bf2285ffdfe4562fb8b3e53f043dc824fb235
Gonzalo Peralta <gonnzaperalta94@gmail.com>
2026-05-15 00:01:47 +0200

    feat: add swagger and documentation app

--- src/server/server.js
+++ src/server/server.js
@@ -5,11 +5,91 @@ import swaggerUi from "swagger-ui-express";
 import userRouter from "../routes/user.router.js";
 import logger from "../logs/logger.js";
 import logsRouter from "../routes/logs.router.js";
-
+import { descriptions } from "jest-config";
 
 dotenv.config();
 
 const app = express();
+
+//swagger documentation
+const swaggerOptions = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Api De Usuarios",
+            version: "1.0.0",
+            description:
+                "Ejemplode JWT, Rutas publicas/protegidas, Autenticación, Autorización, Crud de usuarios, Logs",
+        },
+        servers: [{ url: "http://localhost:7777" }],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: "http",
+                    scheme: "bearer",
+                    bearerFormat: "JWT",
+                },
+            },
+            schemas: {
+                User: {
+                    type: "object",
+                    properties: {
+                        id: {
+                            type: "string",
+                            description: "ID del usuario",
+                            example: "1",
+                        },
+                        email: {
+                            type: "string",
+                            description: "Email del usuario",
+                            example: "Gonzalo@mail.com",
+                        },
+                        username: {
+                            type: "string",
+                            description: "Nombre del usuario",
+                            example: "Gonzalo",
+                        },
+                    },
+                },
+                LoginResponse: {
+                    type: "object",
+                    properties: {
+                        token: {
+                            type: "string",
+                            description: "Token de autenticación",
+                            example: "asdasdhojoi21nckxzpckasd12421054956.....",
+                        },
+                        user: { $ref: "#/components/schemas/User" },
+                    },
+                },
+                Error: {
+                    type: "object",
+                    properties: {
+                        message: {
+                            type: "string",
+                            description: "Mensaje de error",
+                            example: "Error de ejemplo",
+                        },
+                    },
+                },
+            },
+        },
+    },
+    apis: ["./src/routes/*.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerSpec, {
+        swaggerOptions: {
+            docExpansion: "none",
+            persistAuthorization: false,
+        },
+    }),
+);
 
 app.use(express.json());
 
@@ -24,8 +104,6 @@ app.use("/api", userRouter);
 
 app.use("/logs", logsRouter);
 
-
-
 const rawPort = process.env.PORT;
 const PORT = rawPort ? Number(rawPort) : 7777;
 
@@ -34,5 +112,7 @@ if (Number.isNaN(PORT)) {
 }
 
 export function startServer() {
-    app.listen(PORT, () => {logger.info(`Server is running on PORT ${PORT}`) });
+    app.listen(PORT, () => {
+        logger.info(`Server is running on PORT ${PORT}`);
+    });
 }
